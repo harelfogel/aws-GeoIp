bf4dcf0a219057ce0e3bff0538148f2d1d66794c
AmirAvidan <amiravidan0@gmail.com>
2022-05-25 20:20:47 +0300

    fixing lambda, getting fasth key from .env, getting good respnose

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-const express = require('express');
-require('dotenv');
-const axios = require('axios').default;
-const app = express();
-const port = 8080;
-
-app.get('/:IP', (req, res) => {
-//   res.send('Hello World!')
-  axios.get(`https://ep.api.getfastah.com/whereis/v1/json/${req.params.IP}`,{headers: {'Fastah-Key': `${process.env.FASTAH_KEY}`}})
-  .then(function (response) {
-    // handle success
-    // console.log(response);
-    res.send(JSON.stringify(response.data));
+exports.handler = async (event) => {
+  const axios = require('axios');
+  let position = event.pathParameters;
+  let ip = event.queryStringParameters.ip;
+  let response = {};
+  await axios({method: 'get',
+      url: 'https://ep.api.getfastah.com/whereis/v1/json/' + ip,
+      headers:{'Fastah-Key': process.env.FastahKey}
+  }).then((res) => {
+      response = {
+          statusCode: 200,
+          body: JSON.stringify(res.data)
+      }
+  }).catch((err) => {
+      response = {
+          statusCode: 400,
+          body:JSON.stringify({
+              message: "Invalid ip"
+          } )
+      }
   })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-//   fetch('https://ep.api.getfastah.com/whereis/v1/json/auto', 
-//         { mode: 'cors',
-//           headers: {'Fastah-Key': '136ea10eb37341fcb01ea4a15a3677be'}
-//         })
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-});
+  return response;
+};
